a6c67eff9f03c701098539d7c8e7086b34817364
AurobindoGupta <aurogpt10@gmail.com>
2021-04-10 00:27:41 +0530

    yo

--- src/App.js
+++ src/App.js
@@ -8,28 +8,31 @@ import './App.css';
 import AddCircleOutlinedIcon from '@material-ui/icons/AddCircleOutlined';
 import { db } from './firebase_configuration';
 import firebase from 'firebase';
+import TodoListItem from './todo';
 
 
 
 function App(){ 
-  const [todos, setTodos] = useState([])
+  const [Btodos, setTodos] = useState([]);
   const [todoInput, setTodoInput] = useState("");
  useEffect(() => {
    getTodos();
    
- }, []) //only running on first launch
- 
+ }, []) ;//only running on first launch
+
  
  function getTodos(){
-   db.collection("todos").onSnapshot(function(querySnapshot){
+   db.collection("todoS").onSnapshot(function(querySnapshot){
      setTodos(
       querySnapshot.docs.map((doc)=>({
-        id:doc.id,
+        id: doc.id,
         todo: doc.data().todo,
         inprogress: doc.data().inprogress,
       }))
      );//when ever new data updated it wil reflect real time db
- })
+ });
+}
+
   function addTodo(e){
     e.preventDefault();
     db.collection("todoS").add({
@@ -41,9 +44,11 @@ function App(){
 
 
   return (
-    <div className="App" style={{textAlign:"center"}}>
+    <div className="App" >
+      <div style={{textAlign:"center"}}>
       <h1>AUROBINDO GUPTA</h1>
       <h4>TODO LIST APP</h4>
+      
       <form>
       <TextField id="standard-basic" label="ENTER TASK." variant="outlined" 
       value={todoInput} 
@@ -54,9 +59,13 @@ function App(){
         <AddCircleOutlinedIcon/>
       </IconButton>
       </form>
-      {todos.map((todo)=>(
-        <p>{todo.todo}</p>
+
+      { Btodos.map ((todo) => (
+       <TodoListItem todo={todo.todo} 
+       inprogress={todo.inprogress} 
+       todo={todo.id}/> 
       ))}
+    </div>
     </div>
   );
 }
